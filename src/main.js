f46d487fbf7fa6ca78b64a5961d977a35012878e
Jesús Franco <jdfrancov2020@gmail.com>
2025-07-03 00:01:00 -0500

    first commit

---
+++ src/main.js
@@ -0,0 +1,32 @@
+import "./style.css";
+
+
+document.querySelector("#app").innerHTML =`
+  <section id="animacion1">
+    <div class="anim">
+      <h1>Animaciones CSS</h1>
+      <div class="cubitos">
+        <div class="cubito"></div>
+        <div class="cubito"></div>
+        <div class="cubito"></div>
+      </div>
+    </div>
+  </section>
+
+  <section id="animacion2">
+    <div class="anim">
+      <div class="anim2">
+        <div class="animdiv2">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur
+          non cumque exercitationem, ipsam explicabo soluta recusandae nisi
+          doloremque autem itaque sit suscipit magni, cupiditate tempore, a
+          quod. Doloribus, reiciendis ipsam.
+        </div>
+      </div>
+    </div>
+  </section>
+      
+`;
+
+
+
